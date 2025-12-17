19a01cee935a078a6ae54188de2b54e0b516c199
daisukehamza <ouriemchihamza2005@gmail.com>
2025-12-17 16:57:34 +0100

    vite confing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: "/HamzaPortfolio/", // remplace NOM_DU_REPO par le nom exact de ton dépôt GitHub
+});
