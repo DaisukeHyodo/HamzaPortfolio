2b98f7a12b22ef1687c031c2a95a119f0da0f128
daisukehamza <ouriemchihamza2005@gmail.com>
2026-03-25 16:33:24 +0100

    vite encore une fois

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio/", // Indispensable pour GitHub Pages
+  base: "/HamzaPortfolio/", // Indispensable pour GitHub Pages
 });
